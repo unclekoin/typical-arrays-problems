28fa3a1843227e87e2cf3bca2e4d1158aaa30872
unclekoin <unclekoin@gmail.com>
2020-09-29 11:58:54 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined) return 0;
+    if(array.length === 0) return 0;
+    
+    let minNumber = 0;
+    for (let i of array) {
+        if(i < minNumber) {
+            minNumber = i;
+        }
+        
+    }
+    return minNumber
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined) return 0;
+    if(array.length === 0) return 0;
+    
+    let maxNumber = 0;
+    for (let i of array) {
+        if(i > maxNumber) {
+            maxNumber = i;
+        }
+        
+    }
+    return maxNumber
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined) return 0;
+    if(array.length === 0) return 0;
+    
+    let sumNumber = 0;
+    for (let i of array) {
+        sumNumber += i;
+    }
+    return sumNumber / array.length
 }
